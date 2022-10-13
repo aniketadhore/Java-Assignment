0ae8e2d2c74d1a98fd0000909ee28e7e2479b65d
unknown <adhoreaniket9429@gmail.com>
2022-10-12 22:03:13 -0700

    assignment 1 add

--- Info.java
+++
@@ -1,8 +0,0 @@
-public class Info {
-    public static void main(String args[])
-    {
-        System.out.println("Adhore Aniket Digambar");
-        System.out.println("Adhorewadi");
-    }
-    
-}
